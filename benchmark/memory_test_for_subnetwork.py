8160e1921b16a02d7ef42d3a489cf1981a2af911
veya2ztn <zhangtianning110@outlook.com>
2021-12-04 02:43:27 +0800

    benchmark script

--- benchmark/memory_test_for_subnetwork.py
+++ benchmark/memory_test_for_subnetwork.py
@@ -3,8 +3,8 @@ path_recorder = "path_recorder.json"
 import pandas as pd
 import torch.utils.benchmark as benchmark
 from tqdm import tqdm
-D_list = [2,4,6,8,10,12]
-L_list = [3,4,5,6,7,8]
+D_list = [2,4]
+L_list = [3,4]
 y,x = np.meshgrid(D_list,L_list)
 y=y.flatten()
 x=x.flatten()
@@ -18,17 +18,17 @@ function_list = [submatrix_test_full_contraction_einsum,
                  submatrix_test_recurrent_contraction_oe
                 ]
 
-
+B=10;
 results = {}
 for L,D in tqdm(LD_List):
     col_name = f"L={L},D={D}"
     for function in tqdm(function_list):
         row_name = function.__name__.replace("matrix_test","")
-        tqdm.write(f"{col_name}:{row_name}")
+        #tqdm.write(f"{col_name}:{row_name}")
         if row_name not in results:results[row_name]={}
         cuda_reset();
         try:
-            cost = function(D=D,L=L,path_recorder=path_recorder)[1]
+            cost = function(D=D,L=L,B=B,path_recorder=path_recorder)[1]
         except:
             cost = "none"
         results[row_name][col_name]=cost
--- benchmark/test_functions.py
+++ benchmark/test_functions.py
@@ -29,6 +29,7 @@ def cuda_reset():
     torch.cuda.reset_accumulated_memory_stats()
     #print(f"empty:{torch.cuda.memory_stats()['reserved_bytes.all.peak']/1024/1024}M"),
 def structure_operands(tensor_list,sublist_list,outlist,type='torch'):
+    type='torch'
     if type == "torch":
         operands=[]
         for tensor,sublist in zip(tensor_list,sublist_list):
@@ -43,17 +44,13 @@ def structure_operands(tensor_list,sublist_list,outlist,type='torch'):
         operands+= [[*outlist]]
     return operands
 
+contractor_pool={"torch":torch.einsum,'oe':oe.contract}
+
 def submatrix_test_full_contraction_einsum(D=5,L=5,W=None,B=1,H=None,
                                             backward=True,
                                             path_recorder=None,
                                             contractor='torch'):
-    if B>1: assert contractor == 'torch'
-    if contractor == 'torch':
-        contract_engine = torch.einsum
-    elif contractor == 'oe':
-        contract_engine = oe.contract
-    else:
-        raise NotImplementedError
+    contract_engine = contractor_pool[contractor]
 
     W = L if W is None else W
     H = L if H is None else H
@@ -67,10 +64,7 @@ def submatrix_test_full_contraction_einsum(D=5,L=5,W=None,B=1,H=None,
     # #last_idx = outlist.pop()
     # #outlist.insert(W-1,last_idx)
     # path,info                      = get_optim_path_by_oe_from_tn(node_list)
-    if B == 1:
-        tensor_list     = [rd_engine(*l) for t in tn2D_shape_list for l in t]
-    else:
-        tensor_list     = [rd_engine(B,*l) for t in tn2D_shape_list for l in t]
+    tensor_list     = [rd_engine(B,*l) for t in tn2D_shape_list for l in t]
 
     assert len(tensor_list)==len(sublist_list)
     operands = structure_operands(tensor_list,sublist_list,outlist,type=contractor)
@@ -85,13 +79,7 @@ def submatrix_test_sequence_contraction_einsum(D=5,L=5,B=1,
                                                 path_recorder=None,
                                                 contractor='torch',
                                                 recurrent = False):
-    if B>1: assert contractor == 'torch'
-    if contractor == 'torch':
-        contract_engine = torch.einsum
-    elif contractor == 'oe':
-        contract_engine = oe.contract
-    else:
-        raise NotImplementedError
+    contract_engine = contractor_pool[contractor]
     L = L-1
     if recurrent and L>2:
         corn,_= submatrix_test_sequence_contraction_einsum(D=D,L=L,B=B,
@@ -104,26 +92,14 @@ def submatrix_test_sequence_contraction_einsum(D=5,L=5,B=1,
 
     tn2D_shape_list = [[(D,D,D)]+[(D,D,D,D)]*(L-1)]
     path,sublist_list,outlist = get_best_path(tn2D_shape_list,store=path_recorder,type='sub')
-    if contractor == 'torch':
-        if B == 1:
-            tensor_list   = [rd_engine(2,*l) for t in tn2D_shape_list for l in t]
-        else:
-            tensor_list   = [rd_engine(2,B,*l) for t in tn2D_shape_list for l in t]
-        assert len(tensor_list)==len(sublist_list)
-        operands    = structure_operands(tensor_list,sublist_list,outlist,type=contractor)
-        edge1,edge2 = contract_engine(*operands,optimize=path).flatten(-2*L,-L-1).flatten(-L,-1)
-    elif contractor == 'oe':
-        assert B == 1
-        tensor_list= [rd_engine(*l) for t in tn2D_shape_list for l in t]
-        operands   = structure_operands(tensor_list,sublist_list,outlist,type=contractor)
-        edge1      = contract_engine(*operands,optimize=path).flatten(-2*L,-L-1).flatten(-L,-1)
-        tensor_list= [rd_engine(*l) for t in tn2D_shape_list for l in t]
-        operands   = structure_operands(tensor_list,sublist_list,outlist,type=contractor)
-        edge2      = contract_engine(*operands,optimize=path).flatten(-2*L,-L-1).flatten(-L,-1)
-
-    cent_tensor   = rd_engine(D,D,D,D) if B == 1 else  rd_engine(B,D,D,D,D)
-    equation   = "ab,cdb,efcg,gha->hedf" if B == 1 else "kab,kcdb,kefcg,kgha->khedf"
-    tensor_l   = [corn ,edge1,cent_tensor,edge2]
+    #if contractor == 'torch':
+    tensor_list   = [rd_engine(2,B,*l) for t in tn2D_shape_list for l in t]
+    assert len(tensor_list)==len(sublist_list)
+    operands    = structure_operands(tensor_list,sublist_list,outlist,type=contractor)
+    edge1,edge2 = contract_engine(*operands,optimize=path).flatten(-2*L,-L-1).flatten(-L,-1)
+    cent_tensor = rd_engine(B,D,D,D,D)
+    equation    = "kab,kcdb,kefcg,kgha->khedf"
+    tensor_l    = [corn ,edge1,cent_tensor,edge2]
     #path_final = oe.contract_path(equation, *tensor_l)[0]
     path_final = get_best_path_via_oe(equation,tensor_l,store=path_recorder)
     out        = contract_engine(equation,*tensor_l, optimize=path_final).flatten(-4,-3).flatten(-2,-1)
@@ -162,13 +138,8 @@ def fullmatrix_test_with_tn(D=5,L=5):
     loss.backward()
     del node_list
     return out,f"{torch.cuda.memory_stats()['reserved_bytes.all.peak']/1024/1024}M"
-def fullmatrix_test_directly(D=5,L=5,path_recorder=None,contractor='torch'):
-    if contractor == 'torch':
-        contract_engine = torch.einsum
-    elif contractor == 'oe':
-        contract_engine = oe.contract
-    else:
-        raise NotImplementedError
+def fullmatrix_test_directly(D=5,L=5,B=2,path_recorder=None,contractor='torch'):
+    contract_engine = contractor_pool[contractor]
     top_shape_list  =   [(D,D)]  + [  (D,D,D) for i in range(L-2)] + [  (D,D)]
     mid_shape_list  =[  [(D,D,D)]+ [(D,D,D,D) for i in range(L-2)] + [(D,D,D)] for _ in range(L-2)]
     bot_shape_list  =   [(D,D)]  + [  (D,D,D) for i in range(L-2)] + [(D,D)]
@@ -179,39 +150,31 @@ def fullmatrix_test_directly(D=5,L=5,path_recorder=None,contractor='torch'):
     # path,info     = get_optim_path_by_oe_from_tn(node_list,store = path_recorder)
     operands=[]
     #tensor_list = [t.tensor for t in node_list]
-    tensor_list = [rd_engine(*l) for t in tn2D_shape_list for l in t]
+    tensor_list = [rd_engine(B,*l) for t in tn2D_shape_list for l in t]
     operands    = structure_operands(tensor_list,sublist_list,outlist,type=contractor)
     out         = contract_engine(*operands,optimize=path)
     loss = out.norm()
     loss.backward()
     return out,f"{torch.cuda.memory_stats()['reserved_bytes.all.peak']/1024/1024}M"
-def fullmatrix_test_corn_4(D=5,L=6,B=1,path_recorder=None,contractor='torch'):
-    if contractor == 'torch':
-        contract_engine = torch.einsum
-    elif contractor == 'oe':
-        contract_engine = oe.contract
-    else:
-        raise NotImplementedError
-    corn,_ = submatrix_test_full_contraction_einsum(D=D,W=L//2+L%2,H=L//2,B=4,backward=False)
+def fullmatrix_test_corn_4(D=5,L=6,B=2,path_recorder=None,contractor='torch'):
+    contract_engine = contractor_pool[contractor]
+    corn,_ = submatrix_test_full_contraction_einsum(D=D,W=L//2+L%2,H=L//2,B=4*B,backward=False)
+    size   = corn.shape[1:]
+    corn   = corn.reshape(B,4,*size)
     if L%2 == 0:
-        corn   = torch.einsum("kab,kbc->kac",corn[[0,2]],corn[[1,3]])
-        out    = torch.einsum("ab,ba->",corn[0],corn[1])
+        corn   = torch.einsum("xkab,xkbc->xkac",corn[:,[0,2]],corn[:,[1,3]])
+        out    = torch.einsum("xab,xba->",corn[:,0],corn[:,1])
     else:
-        cent = rd_engine(D,D,D,D)
-        equation = "iab,jbc,kcd,lda,ijkl->"
-        tensor_l = [corn[0],corn[1],corn[2],corn[3],cent]
+        cent       = rd_engine(B,D,D,D,D)
+        equation   = "xiab,xjbc,xkcd,xlda,xijkl->x"
+        tensor_l   = [corn[:,0],corn[:,1],corn[:,2],corn[:,3],cent]
         path_final = get_best_path_via_oe(equation,tensor_l,store=path_recorder)
         out        = contract_engine(equation,*tensor_l, optimize=path_final)
     loss   = out.norm()
     loss.backward()
     return corn,f"{torch.cuda.memory_stats()['reserved_bytes.all.peak']/1024/1024}M"
-def fullmatrix_test_RG(D=3,L=7,path_recorder=None,contractor='torch'):
-    if contractor == 'torch':
-        contract_engine = torch.einsum
-    elif contractor == 'oe':
-        contract_engine = oe.contract
-    else:
-        raise NotImplementedError
+def fullmatrix_test_RG(D=3,B=2,L=7,path_recorder=None,contractor='torch'):
+    contract_engine = contractor_pool[contractor]
     def get_index(list1,list2):
         x,y=np.meshgrid(list1,list2)
         return y.flatten(),x.flatten()
@@ -220,7 +183,7 @@ def fullmatrix_test_RG(D=3,L=7,path_recorder=None,contractor='torch'):
     mid_shape_list  =[[(D,D,D,1)]+   [(D,D,D,D) for i in range(L-2)] + [(D,1,D,D)] for _ in range(L-2)]
     bot_shape_list  = [(1,D,D,1)]  + [(1,D,D,D) for i in range(L-2)] + [(1,1,D,D)]
     tn2D_shape_list = [top_shape_list]+mid_shape_list+[bot_shape_list]
-    tensor_list     = [[rd_engine(*l) for l in t] for t in tn2D_shape_list ]
+    tensor_list     = [[rd_engine(B,*l) for l in t] for t in tn2D_shape_list ]
 
     while True:
         W = len(tensor_list)
@@ -229,13 +192,13 @@ def fullmatrix_test_RG(D=3,L=7,path_recorder=None,contractor='torch'):
         if W%2 ==1:
             last_line_dw = tensor_list.pop()
             last_line_up = tensor_list.pop()
-            tensor_list.append([oe.contract("abcd,kjal->kbjcdl",up,dw).flatten(-2,-1).flatten(1,2)
+            tensor_list.append([oe.contract("xabcd,xkjal->xkbjcdl",up,dw).flatten(-5,-4).flatten(-2,-1)
                     for up,dw in zip(last_line_up,last_line_dw)])
         if H%2 ==1:
             tensor_list  = list(map(list,zip(*tensor_list)))
             last_line_dw = tensor_list.pop()
             last_line_up = tensor_list.pop()
-            tensor_list.append([oe.contract("abcd,efgb->aefcgd",up,dw).flatten(-3,-2).flatten(0,1)
+            tensor_list.append([oe.contract("xabcd,xefgb->xaefcgd",up,dw).flatten(-6,-5).flatten(-3,-2)
                     for up,dw in zip(last_line_up,last_line_dw)])
             tensor_list  = list(map(list,zip(*tensor_list)))
         W = len(tensor_list)
@@ -256,11 +219,11 @@ def fullmatrix_test_RG(D=3,L=7,path_recorder=None,contractor='torch'):
 
         tensor_list = []
         for lu,ld,ru,rd in zip(lu_tensor,ld_tensor,ru_tensor,rd_tensor):
-            equation = "abcd,kjal,efgb,hiej->khficgdl"
+            equation = "xabcd,xkjal,xefgb,xhiej->xkhficgdl"
             tensor_l = [lu,ld,ru,rd]
             path = get_best_path_via_oe(equation,tensor_l,store=path_recorder)
             out  = contract_engine(equation,*tensor_l, optimize=path)
-            out  = out.flatten(2,3).flatten(-4,-3).flatten(0,1).flatten(-2,-1)
+            out  = out.flatten(3,4).flatten(-4,-3).flatten(1,2).flatten(-2,-1)
             tensor_list.append(out)
         #print([t.shape for t in tensor_list])
         n = W//2
@@ -269,7 +232,7 @@ def fullmatrix_test_RG(D=3,L=7,path_recorder=None,contractor='torch'):
     loss = out.norm()
     loss.backward()
     return out,f"{torch.cuda.memory_stats()['reserved_bytes.all.peak']/1024/1024}M"
-def fullmatrix_test_boundary(D=3,L=7,W=None,H=None):
+def fullmatrix_test_boundary(D=3,L=7,B=2,W=None,H=None,path_recorder=None):
     # matrix production only, no optimized path
     W = L if W is None else W
     H = L if H is None else H
@@ -284,29 +247,29 @@ def fullmatrix_test_boundary(D=3,L=7,W=None,H=None):
         mid_shape_list   = [  (D,D,D,1)]  + [  (D,D,D,D) for i in range(H-2)] + [(D,1,D,D)]
         tn2D_shape_list += [mid_shape_list]
 
-    tensor_list     = [[rd_engine(*l) for l in t] for t in tn2D_shape_list ]
+    tensor_list     = [[rd_engine(B,*l) for l in t] for t in tn2D_shape_list ]
     tensor_list     = tensor_list[::-1]
     tensor          = tensor_list.pop()
     for i in range(1,H//2):
-
-        tensor=[torch.einsum("zabcd,zkjal->zkbjcdl",up,dw).flatten(-2,-1).flatten(2,3)
+        tensor=[torch.einsum("xzabcd,xzefag->xz e bf c dg",up,dw).flatten(-5,-4).flatten(-2,-1)
                                                         for up,dw in zip(tensor,tensor_list.pop())]
-
-    upper = [t[0] for t in tensor]
-    lower = [t[1] for t in tensor]
+    upper = [t[:,0] for t in tensor]
+    lower = [t[:,1] for t in tensor]
     #upper (D,D^n,1,1) (D,D^n,1,D^n) (D,D^n,1,D^n) (D,1,1,D^n)
     #lower (D,D^n,1,1) (D,D^n,1,D^n) (D,D^n,1,D^n) (D,1,1,D^n)
     if len(tensor_list)==1:
-        #center (2,D,D,D,1) (2,D,D,D,D)  (2,D,D,D,D) (2,D,1,D,D)
-        upper=[torch.einsum("abcd,kjal->kbjcdl",up,dw).flatten(-2,-1).flatten(1,2)
+        #center (D,D,D,1) (D,D,D,D)  (D,D,D,D) (D,1,D,D)
+        upper=[torch.einsum("xabcd,xkjal->x k bj c dl",up,dw).flatten(-5,-4).flatten(-2,-1)
                                                 for up,dw in zip(upper,tensor_list.pop())]
-    tensor=[torch.einsum("abcd,aefg->fbecdg",up,dw).flatten(-2,-1).flatten(1,2)
+
+    tensor=[torch.einsum("xabcd,xaefg->x f be c dg",up,dw).flatten(-5,-4).flatten(-2,-1)
                                                 for up,dw in zip(upper,lower)]
-    tensor= [t.squeeze(0).squeeze(-2) for t in tensor]
-    tensor= tensor[::-1]
+    tensor= [t.squeeze() for t in tensor]
+    if B == 1:tensor= [t.unsqueeze(0) for t in tensor]
     out   = tensor[0]
-    for t in range(1,len(tensor)):
-        out = torch.einsum("ab,cb->ac",out,tensor.pop())
+    for t in range(1,len(tensor)-1):
+        out = torch.einsum("xa,xab->xb",out,tensor[t])
+    out = torch.einsum("xa,xa->x",out,tensor[-1])
     out  = out.squeeze()
     loss = out.norm()
     loss.backward()
