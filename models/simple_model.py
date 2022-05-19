b88214966e96f96a59854de298d76d5442e43698
veya2ztn <zhangtianning110@gmail.com>
2022-05-19 13:03:55 +0800

    split ipynb into small ipynb to accelerate loading

--- models/simple_model.py
+++ models/simple_model.py
@@ -60,3 +60,91 @@ class MPSLinear(nn.Module):
         tensor  = torch.einsum('bip,oplt,bli->bot',left_tensors,self.cent_tensors,rigt_tensors)
         # (NB,b,b) <-> (T,b,b,o) <-> (NB,b,b) ==> (NB,T,t)
         return tensor
+
+class PEPSLinear(nn.Module):
+    '''
+     PEPSLinear(in_features: int, out_features: int, W:int,H:int
+               in_physics_bond: int, out_physics_bond: int, virtual_bond_dim:int,
+                            bias: bool = True, label_position: int or str):
+        input  (Batch, in_features , W, H,  in_physics_bond)
+        output (Batch, out_features,        out_physics_bond)
+    '''
+
+    def __init__(self, in_features,out_features,
+                       in_physics_bond = 2, out_physics_bond=1, virtual_bond_dim=2,
+                       bias=True,label_position='center',init_std=1e-10):
+        super(MPSLinear, self).__init__()
+        if label_position is 'center':label_position = in_features//2
+        assert type(label_position) is int
+        self.in_features   = in_features
+        self.out_features  = out_features
+        self.W             = W
+        self.H             = H
+        self.vbd           = virtual_bond_dim
+        self.ipb           = in_physics_bond
+        self.opb           = out_physics_bond
+
+        self.hn            = label_position
+        left_num           = self.hn
+        right_num          = in_features - left_num
+
+        bias_mat = torch.einsum("ij,kl->ijkl",torch.eye(2),torch.eye(2)).unsqueeze(-1).repeat(1,1,1,1,self.ipb)
+        self.left_tensors = nn.Parameter(init_std * torch.randn(self.W, left_num  ,self.vbd,self.vbd,self.vbd,self.vbd, self.ipb)+ bias_mat)
+        self.rigt_tensors = nn.Parameter(init_std * torch.randn(self.W, right_num ,self.vbd,self.vbd,self.vbd,self.vbd, self.ipb)+ bias_mat)
+
+        bias_mat = torch.einsum("ij,kl->ijkl",torch.eye(2),torch.eye(2)).unsqueeze(-1).repeat(1,1,1,1,self.opb)
+        self.cent_tensors = nn.Parameter(init_std * torch.randn(self.W,self.out_features,self.vbd,self.vbd,self.vbd,self.vbd, self.opb)+ bias_mat)
+
+    @staticmethod
+    def TRG_contraction(tensor):
+        '''
+        Tensor renormalization group Contraction method
+                     |
+           |      —○
+        —●—	==>    \
+           |           ○—
+                       |
+        input: (W[2^N], H[2^N] , D,D,D,D)
+        '''
+        W,H = tensor.shape[:2]
+        lu_tensor = tensor[0:::2,0:::2]
+        ll_tensor = tensor[0:::2,1:::2]
+        ru_tensor = tensor[1:::2,0:::2]
+        rl_tensor = tensor[1:::2,1:::2]
+        size   = int(tensor.shape[0])
+        while size > 1:
+            half_size = size // 2
+            nice_size = 2 * half_size
+            leftover  = tensor[nice_size:]
+            tensor    = torch.einsum("mbik,mbkj->mbij",tensor[0:nice_size:2], tensor[1:nice_size:2])
+            #(k/2,NB,D,D),(k/2,NB,D,D) <-> (k/2,NB,D,D)
+            tensor   = torch.cat([tensor, leftover], axis=0)
+            size     = half_size + int(size % 2 == 1)
+        return tensor.squeeze(0)
+
+    @staticmethod
+    def get_chain_contraction(tensor):
+        size   = int(tensor.shape[0])
+        while size > 1:
+            half_size = size // 2
+            nice_size = 2 * half_size
+            leftover  = tensor[nice_size:]
+            tensor    = torch.einsum("mbik,mbkj->mbij",tensor[0:nice_size:2], tensor[1:nice_size:2])
+            #(k/2,NB,D,D),(k/2,NB,D,D) <-> (k/2,NB,D,D)
+            tensor   = torch.cat([tensor, leftover], axis=0)
+            size     = half_size + int(size % 2 == 1)
+        return tensor.squeeze(0)
+
+    def forward(self, input_data):
+        # the input data shape is (B,L,pd)
+        # expand to convolution patch
+        embedded_data= input_data
+        left_tensors = torch.einsum('wijp,nwp->wnij',self.left_tensors,embedded_data[:,:self.hn])#i.e. (K,NB,b,b)
+        rigt_tensors = torch.einsum('wijp,nwp->wnij',self.rigt_tensors,embedded_data[:,-self.hn:])#i.e.(K,NB,b,b)
+
+        left_tensors = self.get_chain_contraction(left_tensors) #i.e. (NB,b,b)
+        rigt_tensors = self.get_chain_contraction(rigt_tensors) #i.e. (NB,b,b)
+
+        tensor  = torch.einsum('bip,oplt,bli->bot',left_tensors,self.cent_tensors,rigt_tensors)
+        # (NB,b,b) <-> (T,b,b,o) <-> (NB,b,b) ==> (NB,T,t)
+        return tensor
